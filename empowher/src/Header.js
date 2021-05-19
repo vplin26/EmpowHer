8c98b5c9a18bc23474519afad92c9ea1ff81b356
vplin26 <73211557+vplin26@users.noreply.github.com>
2021-05-19 15:39:50 -0700

    set header

---
+++ empowher/src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.css';
+
+function Header(){
+    return (
+        <div className='Header'>
+            <h1>Beep</h1>
+        </div>
+    )
+}
+
+export default Header
